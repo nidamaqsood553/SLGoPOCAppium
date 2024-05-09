c291d85ba784a8539f43d48b09937096ae1081f9
nidamaqsood553 <89477468+nidamaqsood553@users.noreply.github.com>
2024-05-09 11:52:26 +0500

    LoginScript updated

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,2 +1,44 @@
-package tests;public class LoginTest {
-}
+package tests;
+import base.AppiumCapability;
+import io.appium.java_client.AppiumDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import pages.LoginPage;
+
+
+import java.net.URL;
+
+public class LoginTest extends  AppiumCapability{
+    AppiumDriver driver;
+@Test
+        public void login() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("appium:deviceName", "iphone 14");
+        caps.setCapability("platformName", "iOS");
+        caps.setCapability("appium:platformVersion", "16.4");
+        caps.setCapability("appium:automationName", "XCUITest");
+        caps.setCapability("appium:udid", "F55BEF6F-0CCC-4B5A-8DF2-1B6B93D9C108");
+        caps.setCapability("appium:bundleId", "com.discorp.mobile.servicelogistics");
+        AppiumDriver<MobileElement> driver = new IOSDriver<>(new URL("http://localhost:4723"), caps);
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.enterEnterprise();
+        loginPage.enterUsername();
+        loginPage.enterPassword();
+
+        loginPage.clickLogin();
+    System.out.println("Logged in successfully");
+
+
+    }
+
+
+
+    }
